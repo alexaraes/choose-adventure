3eff109e11fc796239507008ea2261ab3cc92508
Alexa Schreffler <alexa.schreffler@gmail.com>
2015-05-13 22:10:26 -0500

    finished adventure

--- dist/game.js
+++ dist/game.js
@@ -1,56 +1,4 @@
 
-
-// console.log(Math.random());
-
-// console.log('hello world');
-
-// console.log('alexa' + ' ' + 'rae');
-
-// console.log('3'+'7');
-
-// console.log(parseInt('8')+9);
-
-// console.log(parseFloat(5.6)+5);
-
-// console.log('Alexa rae'.substring(0,5));
-
-// var myName = 'Alexa Rae';
-
-// console.log(myName);
-
-// myName = 'Alexa';
-
-// console.log(myName);
-
-// console.log(myName + ' totally knows what she is doing');
-
-// console.log(true && true);
-// console.log(false && true);
-// console.log(false && false);
-// // requires both values to be true
-
-// console.log(true || false);
-// // requires only one value to be true
-
-// console.log(true || false && true);
-// // will do or statement first, then and statement
-
-// console.log(5>7);
-// console.log(5>=5);
-// console.log(5 === 7);
-
-// var myAge = 24;
-
-// if(myAge <= 9) {
-// 	console.log('Drink milk.');
-// }
-// else if(myAge <= 20) {
-// 	console.log('drink soda')
-// }
-// else {
-// 	console.log('Get drunk!');
-// }
-
 function* chooseYourAdventure() {
 
 	yield adventure.start('We\'re going on a safari!');
@@ -68,10 +16,11 @@ function* chooseYourAdventure() {
 		var randomNumber = Math.floor(Math.random()*5);
 
 		if(randomNumber < 3) {
-			yield adventure.say('You encountered a giraffe');
+			yield adventure.say('A lion came and ate you! Oh no.');
 			}
 		else {
-			yield adventure.choose('What would you like to do next?', 'Climb a mountain', 'Swim in a river', 'Go home');
+			yield adventure.say('A tribe adopted you and you lived happily ever after.');
 		}
 	}
+
 }
